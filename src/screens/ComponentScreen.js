d021596169baf31338d7a4fc2bb0a9eb1ae5ec50
Gerald Senarclens de Grancy <oss@senarclens.eu>
2022-04-28 11:07:05 +0200

    implemented first task

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+
+const ComponentsScreen = () => {
+  let name = 'Edgar';
+  return <View>
+    <Text style={styles.textStyleHeader}>Getting Started with React Native</Text>
+    <Text>My name is {name}</Text>
+  </View>;
+}
+
+const styles = StyleSheet.create({
+  textStyleHeader: {
+    fontSize: 45,
+  },
+  textStyle: {
+    fontSize: 20,
+  }
+});
+
+export default ComponentsScreen;
